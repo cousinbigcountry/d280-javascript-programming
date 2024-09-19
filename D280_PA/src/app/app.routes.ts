93fb3b2f9b0b16123745e2aa6ba7db0c7a3b7fd4
Kylef12 <kyle.c.fountain@gmail.com>
2024-09-18 21:50:04 -0400

    B

--- D280_PA/src/app/app.routes.ts
+++ D280_PA/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component'
+import { MapComponent } from './map/map.component'
+import { AboutComponent } from './about/about.component'
+import { ContactComponent } from './contact/contact.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'map', component: MapComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'}
+];

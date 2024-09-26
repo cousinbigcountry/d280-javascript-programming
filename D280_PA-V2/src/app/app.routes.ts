0b77dc0ee98454bffd918934924f69c078f3f19a
Kylef12 <kyle.c.fountain@gmail.com>
2024-09-25 23:06:00 -0400

    D

--- D280_PA-V2/src/app/app.routes.ts
+++ D280_PA-V2/src/app/app.routes.ts
@@ -1,4 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 import { MapComponent } from './map/map.component';
 
-export const routes: Routes = [{ path: 'map', component: MapComponent }];
+export const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'home', component: HomePageComponent },
+  { path: 'map', component: MapComponent },
+];

7df02edb26e29a80a718bd73cc7178716f73d31b
Kylef12 <kyle.c.fountain@gmail.com>
2024-09-25 21:05:29 -0400

    Reset

--- D280_PA-V2/src/app/app.component.spec.ts
+++ D280_PA-V2/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'D280_PA' title`, () => {
+  it(`should have the 'D280_PA-V2' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('D280_PA');
+    expect(app.title).toEqual('D280_PA-V2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, D280_PA');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, D280_PA-V2');
   });
 });
--- D280_PA/README.md
+++
@@ -1,5 +0,0 @@
-Student ID: 011556792
-Angular CLI: 17.3.9
-Node: 22.8.0 (Unsupported)
-Package Manager: npm 10.8.2
-OS: win32 x64

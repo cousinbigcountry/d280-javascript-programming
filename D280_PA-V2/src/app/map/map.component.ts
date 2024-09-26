c18cc6e2a1730e03bdda110182b24a3ecb60ffba
Kylef12 <kyle.c.fountain@gmail.com>
2024-09-26 00:39:40 -0400

    F

--- D280_PA-V2/src/app/map/map.component.ts
+++ D280_PA-V2/src/app/map/map.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-map',
   standalone: true,
   imports: [],
   templateUrl: './map.component.html',
-  styleUrl: './map.component.css'
+  styleUrl: './map.component.css',
 })
-export class MapComponent {
+export class MapComponent implements OnInit {
+  ngOnInit(): void {
+    let svgPaths = document.querySelectorAll<SVGPathElement>('path');
+    Array.prototype.forEach.call(svgPaths, (svgCount: SVGPathElement) => {
+      svgCount.addEventListener('mouseover', (event: MouseEvent) => {
+        const path = event.target as SVGPathElement;
+        path.style.fill = '#ffffff';
+      });
 
+      svgCount.addEventListener('mouseleave', (event: MouseEvent) => {
+        const path = event.target as SVGPathElement;
+        path.style.fill = '';
+      });
+
+      svgCount.addEventListener('click', () => {
+        this.loadData(svgCount);
+      });
+    });
+  }
 }
